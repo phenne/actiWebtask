6eb0ce19b67deb163f5cae70b1a5798fd0ae6c61
phenne <gufimy@mail.ru>
2017-11-10 18:41:37 +0300

    711.101.841

--- src/main/java/bd/DataSourceGetter.java
+++ src/main/java/bd/DataSourceGetter.java
@@ -1,16 +1,10 @@
 package bd;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
-
 import javax.sql.DataSource;
 
 
 public class DataSourceGetter {
-
-//    // TODO: 10.11.2017 скрипты и конфиги
-//    private static final String URL = "jdbc:mysql://localhost:3306/user_list?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-//    private static MysqlDataSource dataSource = null;
-
+    
     private DataSourceGetter() {
     }
 
